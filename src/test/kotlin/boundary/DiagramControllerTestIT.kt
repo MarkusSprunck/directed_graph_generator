9443fa589d5cf682f6f623e7d34a6db4a889b1b0
MarkusSprunck <markus.sprunck@mnet-online.de>
2018-11-01 17:07:59 +0100

    Suppress warning

--- src/test/kotlin/boundary/DiagramControllerTestIT.kt
+++ src/test/kotlin/boundary/DiagramControllerTestIT.kt
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.test.context.junit4.SpringRunner
 
-@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS", "RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = [MainApplication::class], webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class DiagramControllerTestIT {
