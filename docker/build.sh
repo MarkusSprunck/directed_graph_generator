21fad2026a17729282ada2b42af4751ce8fe534e
MarkusSprunck <markus.sprunck@mnet-online.de>
2020-11-26 11:21:15 +0100

    Just create and tag image (don't start container during build)

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-docker container stop dgg
-docker container rm dgg
 docker build --no-cache -t dgg-image:latest ../.  --rm=true
-docker run -d -p 8080:8080  -it  --name  dgg  dgg-image:latest
 docker tag dgg-image:latest ecs-globpidck01.otc.ihkmun/dgg/bl:1.1.0
 
