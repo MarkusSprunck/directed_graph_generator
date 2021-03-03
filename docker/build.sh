82adaf6df88781235abfb41e7672f0582df5e189
MarkusSprunck <markus.sprunck@mnet-online.de>
2021-03-03 09:34:39 +0100

    - add support to change the url in diagram links

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker build --no-cache -t dgg-image:latest ../.  --rm=true
-docker tag dgg-image:latest ecs-globpidck01.otc.ihkmun/dgg/bl:1.2.0
+docker tag dgg-image:latest ecs-globpidck01.otc.ihkmun/dgg/bl:1.3.0
 
