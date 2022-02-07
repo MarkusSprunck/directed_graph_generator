4b24126bae2303d56de557daaeb3b6809ddc635b
MarkusSprunck <markus.sprunck@mnet-online.de>
2022-02-07 12:44:52 +0100

    Fix build

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,4 @@
 docker build --no-cache -t sprunck/directed-graph-generator:latest ../.  --rm=true
 docker tag sprunck/directed-graph-generator:latest sprunck/directed-graph-generator:v3
 docker tag sprunck/directed-graph-generator:latest ecs-globpidck01.otc.ihkmun/dgg/bl:1.4.0
-docker push sprunck/directed-graph-generator:v3
-docker push sprunck/directed-graph-generator:latest
 
